206b8e4337f69a3a119920c51c745838c9fc7042
Denzale Reese <dlr249@cornell.edu>
2016-09-08 14:34:24 -0700

    ready for heroku deploy

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/project-one');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project-one');
 
 module.exports.Listing = require('./listing.js');
 module.exports.Contact = require('./contact.js');
