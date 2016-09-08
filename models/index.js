42d40d9044838f55bbe08981661280ff0c9e1423
Denzale Reese <dlr249@cornell.edu>
2016-09-08 13:42:15 -0700

    adds Contact schema

--- models/index.js
+++ models/index.js
@@ -2,3 +2,4 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/project-one');
 
 module.exports.Listing = require('./listing.js');
+module.exports.Contact = require('./contact.js');
